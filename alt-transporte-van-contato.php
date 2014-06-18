4d9c053f146ea2c45296c36b85e25bcb0d4ad4b2
fredchien <fred_chien@yahoo.com.br>
2014-06-18 17:49:14 -0300

    formulario de contato
    contato

--- alt-transporte-van-contato.php
+++ alt-transporte-van-contato.php
@@ -1,39 +1,45 @@
 <?php
 
 if (isset($_POST['submit']) && ($_POST['submit']=='Enviar')){
-	
-	/* Verifica qual é o sistema operacional do servidor para ajustar o cabeçalho de forma correta. Não alterar */
-	if(PHP_OS == "Linux") $quebra_linha = "\n"; //Se for Linux
-	elseif(PHP_OS == "WINNT") $quebra_linha = "\r\n"; // Se for Windows
-	else die("Este script nao esta preparado para funcionar com o sistema operacional de seu servidor");
 
+	include("phpmailer/class.phpmailer.php");
+	
+	$mail = new PHPMailer();
 	$nome = strip_tags(htmlentities($_POST['nomecontato'], ENT_QUOTES,'UTF-8'));
-	$email = strip_tags($_POST['emailcontato']);
+	$email = $_POST['emailcontato'];
 	$telefone = strip_tags($_POST['fonecontato']);
 	$tipo = strip_tags($_POST['tipo']);
+	
 	$mensagem = strip_tags(htmlentities($_POST['mensagem'], ENT_QUOTES,'UTF-8'));
 	
-	/* Montando o cabeçalho da mensagem */
-	$headers = "MIME-Version: 1.1".$quebra_linha;
-	$headers .= "Content-type: text/html; charset=iso-8859-1".$quebra_linha;
-	// Perceba que a linha acima contém "text/html", sem essa linha, a mensagem não chegará formatada.
-	$headers .= "From: ".$email.$quebra_linha;
-	$headers .= "Return-Path: " . $email.$quebra_linha;
-  
-  
-	$to = 'fred@ifwebs.com'; 
-	$subject = 'Contato enviado pelo website'; 
-	$msg = "Oi, veja abaixo o contato realizado pelo site: \n" . 
-	        "Nome: $nome \n" . 
-	        "E-mail: $email \n" . 
-	        "Telefone: $telefone \n" . 
-	        "Mensagem: $mensagem \n"; 
-          
-  
-	mail($to, $subject, $msg, $headers,"-r".$email); 
+	$assunto = "{$tipo} - Contato site";
 	
+	$mensagemHTML  = '<p>Nome: '.$nome.'</p>';
+	$mensagemHTML .= '<p>E-mail: '.$email.'</p>';
+	$mensagemHTML .= '<p>Telefone: '.$telefone.'</p>';
+	$mensagemHTML .= '<p>Mensagem: '.$mensagem.'</p>';
+	$mensagemHTML .= '<p>Data: '.date("d/m/Y H:i").'</p>';
+		
+	$mail->IsSMTP(); // mandar via SMTP
+	$mail->Host = "smtp.alt.tur.br"; // Seu servidor smtp
+	$mail->SMTPAuth = true; // smtp autenticado
+	$mail->Port = 587; // porta de comunicacao
+	$mail->Username = "teste@alt.tur.br"; // usuÃ¡rio deste servidor smtp
+	$mail->Password = "teste123"; // senha 
 	
-	if(mail){
+	$mail->FromName = $nome;
+	$mail->SetLanguage( 'br', 'phpmailer/language/' ); // Carrega o idioma
+	$mail->From = "teste@alt.tur.br";
+	$mail->AddAddress('teste@alt.tur.br');
+	//$mail->AddCC($email);
+	//Endereço de resposta
+	$mail->AddReplyTo($email);
+	$mail->WordWrap = 50; // set word wrap
+	$mail->IsHTML(true); // send as HTML
+	$mail->Subject = '=?ISO-8859-1?B?' . base64_encode($assunto) . '?=';
+	$mail->Body = utf8_encode($mensagemHTML);
+	
+	if($mail->Send()){
 		echo '<script>alert("Mensagem enviada com sucesso!")</script>';
 	}else{
 		echo '<script>alert("Erro ao enviar a mensagem!")</script>';
@@ -119,10 +125,10 @@ if (isset($_POST['submit']) && ($_POST['submit']=='Enviar')){
 						<option name="Sugestão">Sugestão</option>
 						<option name="Reclamação">Reclamação</option>
 					</select>
-					<input name="nomecontato" id="nomecontato" type="text" placeholder="* Nome" required="required">
-					<input name="emailcontato" id="emailcontato" type="email" placeholder="* Email">
-					<input name="fonecontato" id="fonecontato" type="text" placeholder="* Fone Contato">
-					<textarea name="mensagem" placeholder="* Mensagem"></textarea>
+					<input name="nomecontato" id="nomecontato" type="text" placeholder="* Nome" required>
+					<input name="emailcontato" id="emailcontato" type="email" placeholder="* Email" required>
+					<input name="fonecontato" id="fonecontato" type="tel" placeholder="* Fone Contato" required>
+					<textarea name="mensagem" placeholder="* Mensagem" autocomplete="off" required></textarea>
 				</fieldset>
 				
 				
---
+++ email.php
@@ -0,0 +1,31 @@
+﻿<?php 
+
+
+$nome = $_POST['nomecontato'];
+$email = $_POST['emailcontato'];
+$fone = $_POST['fonecontato'];
+$tipo = $_POST['tipo'];
+$mensagem = $_POST['mensagem'];
+$headers = "MIME-Version: 1.1\n";
+$headers .= "Content-type: text/plain; charset=utf-8\n";
+$headers .= "From: $email \n"; // remetente
+$headers .= "Return-Path: $email \n"; // return-path
+$headers .= "Reply-To: $email \n"; // Endereço (devidamente validado) que o seu usuário informou no contato
+
+
+$to = 'contato@ifwebs.com';
+$subject = '[Ifwebs] Contato';
+$msg = "Oi, veja abaixo o contato realizado pelo site: \n" .
+		"Nome: $nome \n" .
+		"E-mail: $email \n" .
+		"Mensagem: $mensagem \n";
+		
+
+mail($to, $subject, $msg, $headers,$email);
+
+if(mail)
+echo '<script type="text/JavaScript">
+	 	alert("Seu contato foi enviado com sucesso. Obrigado!");
+	 	location.href="index.html"
+   	 </script>';
+?>
